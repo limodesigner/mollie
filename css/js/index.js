50dde1665baebaae9772b5bfcc88554c78028d47
limodesigner <linda@digitaldesigner.no>
2023-07-23 21:26:33 +0200

    update 1.1

--- css/js/index.js
+++ css/js/index.js
@@ -1,13 +1,28 @@
+// @author Linda Moenstre <linda@digitaldesigner.no>
+
+const productsContainer = document.querySelector(".products");
 baseUrl = "https://mollie.no/wp-json/wc/v3/products/";
 
-async function getProducts(url) {
-  const response = await fetch(url);
-  const products = await response.json();
-  console.log(products);
+async function getProducts() {
+  try {
+    const response = await fetch(baseUrl);
+    const products = await response.json();
+
+    productsContainer.innerHTML = "";
+
+    products.forEach(function (products) {
+      productsContainer.innerHTML += `
+      <a href="index.html?id=${products.id}" class="product-item">      
+          <img src="${products.images}" />
+          <h4>${products.name}</h4>
+          <p class="price">&euro; ${products.price}</p>
+                <p class="button_category">View more</p>
+      </a>
+      `;
+    });
+  } catch (error) {
+    console.error({ error: "Could not load content" });
+  }
 }
 
-getProducts(baseUrl);
-
-//copyright year
-const d = new Date();
-document.getElementById("currentYear").innerHTML = d.getFullYear();
+getProducts();
